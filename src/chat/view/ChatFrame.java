82a702a49a1413437154dc65fbaaffc074f0eb84
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-11-15 12:22:40 -0700

    fixed some errors

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,5 +5,23 @@ import javax.swing.JFrame;
 
 public class ChatFrame extends JFrame
 {
-
+	private ChatController appController;
+	private ChatPanel appPane;
+	
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		appPane = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+		this.setContentPane(appPane);
+		this.setTitle("Chatting with the Chatbot");
+		this.setResizable(false);
+		this.setSize(600, 600);
+		this.setVisible(true);
+	}
 }

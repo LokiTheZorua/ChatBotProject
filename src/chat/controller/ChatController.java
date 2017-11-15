9894b81347916e177c4a08f77fc17fbdb90afba0
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-11-15 12:29:21 -0700

    made it so the panel would form

--- src/chat/controller/ChatController.java
+++ src/chat/controller/ChatController.java
@@ -2,27 +2,30 @@ package chat.controller;
 
 import chat.model.Chatbot;
 import chat.view.PopupDisplay;
+import chat.view.ChatFrame;
 
 public class ChatController
 {
 	private Chatbot chatbot;
 	private PopupDisplay display;
+	private ChatFrame appFrame;
 	
 	public ChatController()
 	{
 		chatbot = new Chatbot("Alec Jones");
 		display = new PopupDisplay();
+		appFrame = new ChatFrame(this);
 	}
 	
 	public void start()
 	{
 		String response = display.collectResponse("What do you want to talk about?");
 		
-		while (chatbot.lengthChecker(response)  && !chatbot.quitChecker(response))
-		{
-			response = popupChat(response);
-			response =  display.collectResponse(response);
-		}
+//		while (chatbot.lengthChecker(response)  && !chatbot.quitChecker(response))
+//		{
+//			response = popupChat(response);
+//			response =  display.collectResponse(response);
+//		}
 	}
 	
 	private String popupChat(String chat)

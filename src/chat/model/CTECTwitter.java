8bed28566583ccd08919a6d3c1fd9656719194e6
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2018-03-01 11:50:46 -0700

    fixed imports and made methods

--- src/chat/model/CTECTwitter.java
+++ src/chat/model/CTECTwitter.java
@@ -1,17 +1,23 @@
 package chat.model;
 
 import chat.controller.ChatbotController;
+import chat.controller.IOController;
 
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.Twitter;
-import twitter4j.Status;
+import twitter4j.*;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.text.DecimalFormat;
 
 public class CTECTwitter
 {
 
 	private ChatbotController appController;
 	private Twitter chatbotTwitter;
+	private List<Status> searchedTweets;
+	private List<String> tweetedWords;
+	private long totalWordCount;
 	
 	public CTECTwitter(ChatbotController appController)
 	{
